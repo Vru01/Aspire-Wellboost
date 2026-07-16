e6490338219b78f6e9a2667708cc6534783e3c42
Vru01 <vrushalisangale16@gmail.com>
2026-07-17 02:36:02 +0530

    connected to frontend

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["https://aspire-wellboost-frontend.onrender.com"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
